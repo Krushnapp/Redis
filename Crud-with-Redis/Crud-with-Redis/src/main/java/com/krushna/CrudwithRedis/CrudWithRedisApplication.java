b55f56934cbcf071ecd521c08653126905ed53d5
Krushnapp <krush15699@gamail.com>
2023-11-05 21:52:00 +0530

    New Repo

---
+++ Crud-with-Redis/Crud-with-Redis/src/main/java/com/krushna/CrudwithRedis/CrudWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.krushna.CrudwithRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithRedisApplication.class, args);
+	}
+
+}
